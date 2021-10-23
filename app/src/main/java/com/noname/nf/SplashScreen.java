fe6b1897d413dbeae8da6bcafa93a970308ba24b
DimasAjisaka <4anakcyber@gmail.com>
2021-10-23 19:49:30 +0700

    add splash screen

--- app/src/main/java/com/noname/nf/SplashScreen.java
+++ app/src/main/java/com/noname/nf/SplashScreen.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    // variables
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

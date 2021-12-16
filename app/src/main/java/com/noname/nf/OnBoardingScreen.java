439b6c6ef70fd6a0f41c42bee1c7109cbcab36de
alter0z <afilaansori2602@gmail.com>
2021-12-16 17:55:40 +0700

    edit splash screen -> change background color, add frame layout, add color, add logo,create OnBoardingScreen

---
+++ app/src/main/java/com/noname/nf/OnBoardingScreen.java
@@ -0,0 +1,14 @@
+package com.noname.nf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_screen);
+    }
+}
--- app/src/main/java/com/noname/nf/SplashScreen.java
+++ app/src/main/java/com/noname/nf/SplashScreen.java
@@ -3,6 +3,7 @@ package com.noname.nf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,7 +12,10 @@ import android.os.Handler;
 public class SplashScreen extends AppCompatActivity {
 
     // variables
+    SharedPreferences onBoardingScreen;
 //    FirebaseAuth auth;
+
+
     private static int SPLASH_SCREEN = 1000;
 
     @Override
@@ -22,6 +26,19 @@ public class SplashScreen extends AppCompatActivity {
 //        auth = FirebaseAuth.getInstance();
 
         new Handler().postDelayed((Runnable) () -> {
+
+            // is first time checked
+            onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+            boolean isFirstTime = onBoardingScreen.getBoolean("firstTime",true);
+
+            if (isFirstTime){
+                SharedPreferences.Editor editor = onBoardingScreen.edit();
+                editor.putBoolean("firstTime",false);
+                editor.apply();
+                startActivity(new Intent(SplashScreen.this,OnBoardingScreen.class));
+                finish();
+
+            } else {
 //            // check auth then to dashboard
 //            if(auth.getCurrentUser() != null){
 //                toMainApp();
@@ -29,6 +46,7 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this,MainPage.class));
                 finish();
 //            }
+            }
         },SPLASH_SCREEN);
     }
 

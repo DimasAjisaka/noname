a5ccbdd7e6444bc52f733cae9508c01f61893e30
alter0z <afilaansori2602@gmail.com>
2021-12-23 23:08:22 +0700

    Create fragments for questions

--- app/src/main/java/com/noname/nf/Questions.java
+++ app/src/main/java/com/noname/nf/Questions.java
@@ -1,14 +1,45 @@
 package com.noname.nf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.noname.nf.fragments.FragmentQ1;
 
 public class Questions extends AppCompatActivity {
+
+    Button next,prev;
+    int questions = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        next = findViewById(R.id.nextQ);
+        prev = findViewById(R.id.prevQ);
+
+        next.setOnClickListener(v -> questions++); // next count question
+        prev.setOnClickListener(v -> questions--); // decrement count to prev question
+
+        // set prev button visibility
+        if (questions > 1) {
+            prev.setVisibility(View.VISIBLE);
+        }
+
+        // here to select question using fragment
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragments,new FragmentQ1());
+        fragmentTransaction.commit();
+
+//        switch (questions) {
+//            case 1:
+//                fragmentTransaction.replace(R.id.fragments,new FragmentQ1());
+//                fragmentTransaction.commit();
+//                break;
+//        }
     }
 }
